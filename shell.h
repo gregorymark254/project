f52674d30322c48314a04c1c1084d2fcc7c97c7c
gregorymark254 <gregorymark254@gmail.com>
2023-08-23 03:26:45 -0700

    shell

--- shell.h
+++ shell.h
@@ -16,30 +16,4 @@
 extern char **environ;
 
 
-
-void getline_sh(char **text);
-void sett(char *text);
-void unsett(char *text);
-void cd(char *text);
-
-void prompt(char **arv, char **envp, bool flg);
-int _strcmp(char *s1, char *s2);
-char *_strcat(char *dest, char *src);
-char *handle_path(char **rgv, char *cmd);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-void handle_exit(char *cmd);
-void runcmd(char **rgv, char **arv, char **envp);
-char *trim(char *cmd);
-char *_strncpy(char *dest, char *src, int n);
-char *get_path(char *cmd);
-char *_getenv(char *name);
-char **tokenize_env(char *path);
-void geterror(denum *n, char **arv, char *cmd);
-
-typedef struct denum
-{
-	int cnt;
-} denum;
-
 #endif
