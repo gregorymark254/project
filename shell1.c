cb6622043f617b33c9049779f8339af063dded57
gregorymark254 <gregorymark254@gmail.com>
2023-08-23 03:53:46 -0700

    updates

--- shell1.c
+++ shell1.c
@@ -1,58 +1,109 @@
 #include "shell.h"
 
 /**
- * create_child - A function that creates a sub process.
- * @command: The pointer to tokenized command
- * @name: The pointer to the name of shell.
- * @env: The pointer to the enviromental variables.
- * @cicles: Number of executed cicles.
- * Return: Nothing.
+ * _strcmp - A funtion that compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 if strings are the same, 0 if not
  */
-void create_child(char **command, char *name, char **env, int cicles)
+int _strcmp(char *s1, char *s2)
 {
-	int pid = 0;
-	int status = 0;
-	int wait_error = 0;
+	unsigned int i = 0;
 
-	pid = fork();
-	if (pid < 0)
+	while (s1[i])
 	{
-		perror("Error: ");
-		free_exit(command);
+		if (s1[i] != s2[i])
+			return (0);
+		i++;
 	}
-	else if (pid == 0)
-	{
-		execute(command, name, env, cicles);
-		free_dp(command);
-	}
-	else
-	{
-		wait_error = waitpid(pid, &status, 0);
-		if (wait_error < 0)
-		{
-			free_exit(command);
-		}
-		free_dp(command);
-	}
+
+	return (1);
 }
 
 
 /**
-  * change_dir - Afunction that changes working directory.
-  * @path: The new current working directory.
-  * Return: 0 on success, -1 on failure.
-  */
-int change_dir(const char *path)
+ * _strlen - A function that finds the length of the string
+ * @s: The input string
+ * Return: The length of the string
+ */
+unsigned int _strlen(char *s)
 {
-	char *buf = NULL;
-	size_t size = 1024;
+	unsigned int len = 0;
 
-	if (path == NULL)
-		path = getcwd(buf, size);
-	if (chdir(path) == -1)
+	while (s[len])
+		len++;
+
+	return (len);
+}
+
+
+/**
+ * _strcpy - A function that copies a string to another string
+ * @dest: The pointer to the copyed string
+ * @src: The pointer to string to copy for
+ * Return: A pointer to copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *aux = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	*dest = '\0';
+	return (aux);
+}
+
+
+/**
+ * _strcat - a function that concatenates two strings.
+ * @dest: an input string
+ * @src: an input string
+ * Return: A pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *temp = dest;
+
+	while (*dest)
+		dest++;
+
+	*dest++ = '/';
+	while (*src)
+		*dest++ = *src++;
+	return (temp);
+}
+
+
+/**
+ * _atoi - a function that converts string to integer.
+ * @s: An input string.
+ * Return: integer from conversion.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int total = 0;
+	char null_flag = 0;
+
+	if (s == NULL)
+		return (0);
+	while (*s)
 	{
-		perror(path);
-		return (98);
+		if (*s == '-')
+			sign *= -1;
+		if (*s >= '0' && *s <= '9')
+		{
+			null_flag = 1;
+			total = total * 10 + (*s - '0');
+		}
+		else if (*s < '0' || *s > '9')
+		{
+			if (null_flag == 1)
+				break;
+		}
+		s++;
 	}
-	return (1);
+	if (sign < 0)
+		total = (-1 * (total));
+	return (total);
 }
