fce9e95437ed3871490d29431a47b35908d584ac
frankrazzih <francischege602@gmail.com>
2023-08-23 09:25:05 -0700

    simple_shell

--- task10.c
+++ task10.c
@@ -4,33 +4,33 @@ int execute(char **args);
 int tokenize(char *text);
 void printenv(void);
 char **args;
-/*extern char **environ;*/
+
 /**
  * main - Entry point of the program
  * Return: 0 indicating success
  */
 int main(void)
 {
-        int forked, waiting;
-        int checker;
-        char *text = NULL;
-       size_t len = 0;
+	int forked, waiting;
+	int checker;
+	char *text = NULL;
+	size_t len = 0;
 	char *ex, *env, *unset, *set, *chdir;
 
-        while (0 < 1)
-        {
-                printf("$ ");
-                checker = getline(&text, &len, stdin);
-                if (checker == EOF)
-                {
-                        printf("\n");
-                        break;
-                }
-                if (checker == -1)
-                {
-                        perror("Line not read\n");
+	while (0 < 1)
+	{
+		printf("$ ");
+		checker = getline(&text, &len, stdin);
+		if (checker == EOF)
+		{
+			printf("\n");
+			break;
+		}
+		if (checker == -1)
+		{
+			perror("Line not read\n");
 			continue;
-                }
+		}
 		ex = strstr(text, "exit");
 		set = strstr(text, "setenv");
 		unset = strstr(text, "unsetenv");
@@ -60,17 +60,18 @@ int main(void)
 		{
 			exit(0);
 		}
-                text[strcspn(text, "\n")] = '\0';
-                forked = fork();
-              if (forked == 0)
-                {
-                        tokenize(text);
-                        execute(args);
-                }
-                wait(&waiting);
-        }
-        return (0);
+		text[strcspn(text, "\n")] = '\0';
+		forked = fork();
+		if (forked == 0)
+		{
+			tokenize(text);
+			execute(args);
+		}
+		wait(&waiting);
+	}
+	return (0);
 }
+
 /**
  * printenv - prints env var
  * Return: void
@@ -85,6 +86,7 @@ void printenv(void)
 		printf("%s", environ[x]);
 	}
 }
+
 /**
  * tokenize - tokenizes the user commands
  * @text: arguments
@@ -92,45 +94,46 @@ void printenv(void)
  */
 int tokenize(char *text)
 {
-        int y = 0;
-        char *delim = " ";
-                args = malloc(sizeof(char*) * 10);
-        if (args == NULL)
-        {
-                perror("Memory allocation failed\n");
-                return (-1);
-        }
-        
-        args[0] = strtok(text, delim);
-        
-        while (args[y] != NULL)
-        {
-                y++;
-        
-                if (y == 10)
-                {
-                        printf("Too many arguments\n");
-                        return (-1);
-                }
-        
-                args[y] = strtok(NULL, delim);
-        }
-        
-        args[y] = NULL;
-        return (0);
-}
+	int y = 0;
+	char *delim = " ";
 
+	args = malloc(sizeof(char *) * 10);
+	if (args == NULL)
+	{
+		perror("Memory allocation failed\n");
+		return (-1);
+	}
+
+	args[0] = strtok(text, delim);
+
+	while (args[y] != NULL)
+	{
+		y++;
+
+		if (y == 10)
+		{
+			printf("Too many arguments\n");
+			return (-1);
+		}
+
+		args[y] = strtok(NULL, delim);
+	}
+
+	args[y] = NULL;
+	return (0);
+}
 
 /**
  * execute - Execute a command using execve
- * @text: The command to execute
+ * @args: The command to execute
  * Return: 0 on success, -1 on error
  */
 int execute(char **args)
 {
 	if (execvp(args[0], args) == -1)
-        {
-                perror("./hsh");
-        }
-        return (0);
+	{
+		perror("./hsh");
+	}
+	return (0);
 }
+
