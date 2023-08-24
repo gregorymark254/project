a9dfc58624297fa456f7049fe9bc16e8ad366d19
gregorymark254 <gregorymark254@gmail.com>
2023-08-24 22:19:20 +0300

    simple shell

--- betty.c
+++ betty.c
@@ -1,9 +1,12 @@
 #include "shell.h"
+
 /**
  * betty - program start
  * Return: 0 indicating success
  */
+ 
 int betty(void)
+
 {
 	printf("Hello betty\n");
 	return (0);
--- shell.h
+++ shell.h
@@ -13,7 +13,7 @@
 #include <errno.h>
 #include <linux/limits.h>
 
-extern char **environ;
+
 
 
 #endif
